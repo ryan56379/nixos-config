634c02e1a763dcbb611295f037e7f8498f56bd36
ryan56379 <94024370+ryan56379@users.noreply.github.com>
2025-04-25 14:51:02 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
 
   #AMD Overclock
   boot.kernelParams = [
