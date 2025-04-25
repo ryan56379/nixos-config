771805257cd114137676f374822bceda04b1e420
ryan56379 <94024370+ryan56379@users.noreply.github.com>
2025-04-25 14:49:37 -0500

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  #AMD Overclock
+  boot.kernelParams = [
+      "amdgpu.ppfeaturemask=0xffffffff"
+  ];
+
   networking.hostName = "gamma"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -44,65 +49,29 @@
 
   #Nvidia environment vars
   environment.variables = {
-    GBM_BACKEND = "nvidia-drm";
-    LIBVA_DRIVER_NAME = "nvidia";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GSK_RENDERER = "gl";
+    LIBVA_DRIVER_NAME = "radeonsi";
+    VDPAU_DRIVER = "radeonsi";
     XCURSOR_THEME = "Adwaita";
   };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  # Load amd driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["amdgpu"];
+
   #Enable graphics with nvidia pkgs
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-        nvidia-vaapi-driver
-        vaapiVdpau
-        libvdpau-va-gl
+        rocmPackages.clr.icd
+        amdvlk
     ];
-  };
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = true;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = true;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
-  };
-
-  # Nvidia suspend issue workaround
-  systemd.services."systemd-suspend" = {
-    serviceConfig = {
-      Environment=''"SYSTEMD_SLEEP_FREEZE_USER_SESSIONS=false"'';
-    };
+    extraPackages32 = with pkgs; [
+        driversi686Linux.amdvlk
+    ];
   };
 
   # Enable the GNOME Desktop Environment.
@@ -196,7 +165,12 @@
    python3
    pipx
    gcc-arm-embedded
+   lact
   ];
+
+  #LACT service
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = ["multi-user.target"];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
